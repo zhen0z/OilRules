1b0c49068f4b0b8dd22b99964edaf94f4b070118
zhen0z <zhen.zhang.cc@gmail.com>
2024-09-17 13:10:29 +0100

    Update jutulForce.jl

--- src/FlowRules/Types/jutulForce.jl
+++ src/FlowRules/Types/jutulForce.jl
@@ -1,3 +1,19 @@
+# export jutulForce
+
+# struct jutulForce{D, T}
+#     irate::T
+#     name::Vector{Symbol}
+#     loc::Vector{NTuple{D, T}}
+# end
+
+# jutulForce(irate::T, loc::NTuple{D, T}) where {D, T} = jutulForce(irate, [loc])
+# jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Producer for i = 1:length(loc)]), loc)
+
+# display(f::jutulForce{D, T}) where {D, T} =
+#     println("$(D)D jutulForce structure with $(length(f.loc)) injection/production wells and rate $(f.irate) m^3/s")
+
+# ==(A::jutulForce{D, T}, B::jutulForce{D, T}) where {D,T} = (A.irate == B.irate && A.name == B.name && A.loc == B.loc)
+
 export jutulForce
 
 struct jutulForce{D, T}
@@ -7,7 +23,7 @@ struct jutulForce{D, T}
 end
 
 jutulForce(irate::T, loc::NTuple{D, T}) where {D, T} = jutulForce(irate, [loc])
-jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat(:Injector, [:Producer for i = 1:length(loc)]), loc)
+jutulForce(irate::T, loc::Vector{NTuple{D, T}}) where {D, T}= jutulForce(irate, vcat([:Injector for i = 1:1], [:Producer for i = 2:length(loc)]), loc)
 
 display(f::jutulForce{D, T}) where {D, T} =
     println("$(D)D jutulForce structure with $(length(f.loc)) injection/production wells and rate $(f.irate) m^3/s")
